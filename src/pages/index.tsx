3a8a0c4d8ed34902277a83c02e2929419d62c260
huonggiangbui <bhuonggiang03@gmail.com>
2021-08-27 20:12:18 -0400

    finalized frontend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,55 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { gql, useMutation } from "@apollo/client";
-import { Button, makeStyles, TextField, Typography, InputAdornment, Tooltip } from "@material-ui/core";
-import { Check as CheckIcon } from "@material-ui/icons";
+import { Button, makeStyles, TextField, Typography, InputAdornment, Tooltip, Accordion, AccordionSummary, AccordionDetails } from "@material-ui/core";
+import { Check as CheckIcon, ExpandMore } from "@material-ui/icons";
 
 import { validateUrl, validateUrlCode } from "../utils/validator";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      width: '50%',
-    },
+    height: '100vh',
+    background: 'linear-gradient(150deg, #E3E1F8 0%, rgba(255,255,255,1) 100%);',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: [
+      "CircularStd",
+      "Roboto",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
   },
-  label: {
-    marginBottom: "8px",
-    marginTop: "16px",
+  form: {
+    padding: theme.spacing(4),
+    width: '50%',
+    background: '#ffffff',
+    boxShadow: '12px 12px 16px 0 #E8E8FA, -8px -8px 12px 0 rgb(255 255 255 / 30%)',
+    borderRadius: '24px',
+  },
+  heading: {
+    padding: "16px 0px",
+    color: "#3E369C",
+    fontSize: "64px",
+    fontWeight: 600,
+  },
+  accordion: {
+    border: 'none',
+    boxShadow: 'none',
+  },
+  accordionComponent: {
+    padding: "0"
+  },
+  subheading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
   },
   tick: {
     fontSize: "15px",
@@ -100,7 +135,7 @@ export default function HomePage() {
     }
 
     return (
-      <Typography className={classes.label}>
+      <Typography className={classes.subheading} style={{ margin: "16px"}}>
         Your shorten URL is: 
         <Tooltip
           title={
@@ -119,16 +154,20 @@ export default function HomePage() {
   }
 
   return (
-    <>
-      <Typography variant="h5" className={classes.label}>
-        Long URL
+    <div className={classes.root}>
+      <Typography variant="h5" className={classes.heading}>
+        Create Short Links!
       </Typography>
-      <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+      <Typography className={classes.subheading} style={{ paddingBottom: "32px"}}>
+        This is a custom short link personalization tool provided by Vietcode Organization that enables you to target, engage, and drive more visitors. Get started now.
+      </Typography>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit} className={classes.form}>
         <TextField
           id="outlined-required"
+          label="Long URL"
           name="longUrl"
           variant="outlined"
-          placeholder="Enter your long url here."
+          placeholder="Paste a link to shorten it"
           required={true}
           fullWidth
           value={input.longUrl}
@@ -144,28 +183,6 @@ export default function HomePage() {
           }
           onChange={handleChange}
         />
-        <div>
-          <TextField
-            id="outlined-required"
-            name="urlCode"
-            variant="outlined"
-            placeholder="Enter your custom url path."
-            fullWidth
-            value={input.urlCode}
-            error={input.urlCode && input.urlCode !== "" ? !valid.urlCode : false}
-            InputProps={
-              valid.urlCode && {
-                endAdornment: (
-                  <InputAdornment position="start">
-                    <CheckIcon className={classes.tick} />
-                  </InputAdornment>
-                ),
-              }
-            }
-            onChange={handleChange}
-          />
-        </div>
-
         <Button
           type="submit"
           variant="contained"
@@ -178,14 +195,48 @@ export default function HomePage() {
           }
           size="large"
         >
-          {loading ? "Loading..." : "Tiếp tục"}
+          {loading ? "Loading..." : "Continue"}
         </Button>
+        <div>
+          <Accordion className={classes.accordion}>
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+              className={classes.accordionComponent}
+            >
+              <Typography className={classes.subheading}>More options</Typography>
+            </AccordionSummary>
+            <AccordionDetails className={classes.accordionComponent}>
+              <TextField
+                id="outlined-required"
+                label="Custom URL Ending"
+                name="urlCode"
+                variant="outlined"
+                placeholder="an_example_ending"
+                fullWidth
+                value={input.urlCode}
+                error={input.urlCode && input.urlCode !== "" ? !valid.urlCode : false}
+                InputProps={
+                  valid.urlCode && {
+                    endAdornment: (
+                      <InputAdornment position="start">
+                        <CheckIcon className={classes.tick} />
+                      </InputAdornment>
+                    ),
+                  }
+                }
+                onChange={handleChange}
+              />
+            </AccordionDetails>
+          </Accordion>
+        </div>
       </form>
       {data 
         ? <ShortUrlComponent />
         : ""
       }
-    </>
+    </div>
   )
 }
 
--- src/pages/redirect.tsx
+++ src/pages/redirect.tsx
@@ -2,10 +2,41 @@ import React, { useEffect } from "react";
 
 import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
-import { Typography } from "@material-ui/core";
+import { makeStyles, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+    background: 'linear-gradient(150deg, #E3E1F8 0%, rgba(255,255,255,1) 100%);',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: [
+      "CircularStd",
+      "Roboto",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
+  heading: {
+    padding: "16px 0px",
+    color: "#3E369C",
+    fontSize: "32px",
+    fontWeight: 600,
+  },
+}));
 
 export default function RedirectPage() {
   let { urlCode } = useParams() as any;
+  const classes = useStyles();
 
   const { data, loading, error } = useQuery(URL_QUERY, {
     variables: {
@@ -21,14 +52,18 @@ export default function RedirectPage() {
 
   if (loading) return null;
   if (error) return (
-    <Typography variant="h5">
-      Error! {error.message}
-    </Typography>
+    <div className={classes.root}>
+      <Typography className={classes.heading} style={{ color: 'red' }}>
+        Error! {error.message}
+      </Typography>
+    </div>
   );
   return (
-    <Typography variant="h5">
-      Going to redirect...
-    </Typography>
+    <div className={classes.root}>
+      <Typography className={classes.heading}>
+        Going to redirect...
+      </Typography>
+    </div>
   )
 }
 
