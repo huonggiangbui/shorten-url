de851b66f5e0909511c8f3d9d4a729fbaa33733d
Giang Bui <bhuonggiang03@gmail.com>
2022-01-07 21:53:31 -0500

    fix warning

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -5,7 +5,7 @@ export function validateUrlCode(urlCode) {
 }
 
 export function validateUrl(url) {
-  let urlRegEx = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+  let urlRegEx = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[\w]*))?)/;
 
   return urlRegEx.test(url);
 }
